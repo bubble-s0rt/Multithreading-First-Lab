900c65b6a290306d30ee8c3f2864e55e79750514
proximaCent <sabikaftahee01@gmail.com>
2024-05-24 03:25:45 +0600

    wait for the threads to finish so added join with a try catch

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
         System.out.println("Hello and welcome!");
 
-        System.out.printf("Initializing banking system..");
+        System.out.println("Initializing banking system..");
 
         int totalNumberOfSimulaion = 10;
         OperationsQueue operationsQueue = new OperationsQueue();
@@ -20,22 +20,32 @@ public class Main {
         simulationThread.start();
 
 
-        System.out.printf("Initializing deposit systen....");
+        System.out.println("Initializing deposit systen....");
         Thread depositThread = new Thread(() -> {
             bank.deposit();
         });
         depositThread.start();
-        System.out.println("coompleted");
+        //System.out.println("coompleted");
 
-        System.out.printf("Initializing withdraw systen....");
+        System.out.println("Initializing withdraw systen....");
         Thread withdrawThread = new Thread(() -> {
             bank.withdraw();
         });
         withdrawThread.start();
-        System.out.println("coompleted");
+        //System.out.println("coompleted"); //message before finishing thread so,
 
 
-        System.out.println("coompleted");
+        //System.out.println("coompleted");
+        
+        try {
+            simulationThread.join();
+            depositThread.join();
+            withdrawThread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Completed");
 
     }
 }
