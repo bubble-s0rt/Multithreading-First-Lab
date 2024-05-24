e1f73ae059075b543e6545191ca852f9d02e4b7c
proximaCent <sabikaftahee01@gmail.com>
2024-05-25 00:03:21 +0600

    tried to imrove the random number generator using java untim random

--- src/OperationsQueue.java
+++ src/OperationsQueue.java
@@ -1,30 +1,39 @@
 import java.util.List;
-import java.util.ArrayList;;
+import java.util.ArrayList;
+import java.util.Random;
+
 public class OperationsQueue {
-    private final List<Integer> operations = new ArrayList<>();
+    private final BlockingQueue<Integer> operations = new LinkedBlockingQueue<>();
 
     public void addSimulation(int totalSimulation) {
 
         // Add 50 random numbers in the operations list. The number will be range from -100 to 100. It cannot be zero.
+        Random random = new Random();
+
         for (int i = 0; i < totalSimulation; i++) {
-            int random = (int) (Math.random() * 200) - 100;
-            if (random != 0) {
-                operations.add(random);
+            // int random = (int) (Math.random() * 200) - 100; this is  not properly optimised, we can use utilities from until.rand
+            int randomnum=random.nextInt(201) - 100;
+            if (randomnum != 0) {
+                operations.add(randomnum);
             }
-            System.out.println(i + ". New operation added: " + random);
+
+            System.out.println(i + ". New operation added: " + randomnum);
             // add small delay to simulate the time taken for a new customer to arrive
+
             try {
-                Thread.sleep((int) (Math.random() * 80));
+                Thread.sleep(random.nextInt(80));
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
         operations.add(-9999);
     }
+
     public void add(int amount) {
         operations.add(amount);
     }
 
+   
     public synchronized int getNextItem() {
         // add a small delay to simulate the time taken to get the next operation.
         while(operations.isEmpty()) {
@@ -36,4 +45,7 @@ public class OperationsQueue {
         }
         return operations.remove(0);
     }
+
+
+
 }
